43cb41c1b2e05ea7ba585ce8ab34f0faf289f63d
SWOPNIL <105921901+swopnil05@users.noreply.github.com>
2022-11-01 14:16:33 +0600

    Add files via upload

---
+++ js/scrip.js
@@ -0,0 +1,11 @@
+$(function(){
+    $('.slider-main').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        arrows: true,
+        prevArrow: '<i class="fa-regular fa-circle-left prev"></i>',
+        nextArrow: '<i class="fa-regular fa-circle-right next"></i>',
+      });
+});
